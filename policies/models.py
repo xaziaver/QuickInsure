9cd241632c4197ae115fa222e3c449bfdb8c4040
xaziaver <61274673+xaziaver@users.noreply.github.com>
2023-10-25 11:18:31 +0000

    initial policy model

--- policies/models.py
+++ policies/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Policy(models.Model):
+    effective_date = models.DateField(default=date.today)
+    expiration_date = models.DateField()
+
+    PRODUCT_CHOICES = [
+        ("HOME", "Homeowner's Insurance"),
+        ("RENT", "Renter's Insurance")
+    ]
+    TYPE_CHOICES = [
+        ("HO3", "HO3"),
+        ("HO4", "HO4"),
+        ("HO6", "HO6")
+    ]
+
+    product = models.CharField(
+        max_length=25,
+        choices=PRODUCT_CHOICES,
+        default=PRODUCT_CHOICES[1],
+    )
+    type = models.CharField(
+        max_length=3,
+        choices=TYPE_CHOICES,
+        default=TYPE_CHOICES[1],
+    )
+    
+    #coverages = 
+    #forms = 
+    
+    #account = 
+
